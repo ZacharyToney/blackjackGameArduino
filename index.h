262090d3a134fa3b5dc44c2097fdd9c57841c966
ZacharyToney <zach@clearseries.com>
2018-04-19 15:37:43 -0400

    added index.h code(html code essentially)
    Signed-off-by: ZacharyToney <zach@clearseries.com>

--- index.h
+++ index.h
@@ -6,28 +6,75 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script class="deckLogic" type="text/javascript">
+	    class Deck{
+	    constructor(){
+	      this.deck = [];
+	      this.reset();
+	      this.shuffle();
+	    }
 
+	    reset(){
+	      this.deck = [];
 
-		<script type="text/javascript">
+	      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+	      const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
 
+	      for (let suit in suits) {
+	        for (let value in values) {
+	          this.deck.push(`${values[value]} of ${suits[suit]}`);
+	        }
+	      }
+	    }
 
+	    shuffle(){
+	      const { deck } = this;
+	      let m = deck.length, i;
 
+	      while(m){
+	        i = Math.floor(Math.random() * m--);
+
+	        [deck[m], deck[i]] = [deck[i], deck[m]];
+	      }
+
+	      return this;
+	    }
+
+	    deal(){
+	      return this.deck.pop();
+	    }
+	  }
+
+	  const deck1 = new Deck();
+	  </script>
+
+		<script class="functionsForTable" type="text/javascript">
+
+			$(document).ready(function(){$(".table").fadeIn(2250,playerStartAndStartButton());});
 
 			function playerStartAndStartButton() {
-				$(".player").fadeIn(2250);
-				$(".startButton").css('display', 'inline-block');
+				setTimeout(
+				  function() 
+				  {
+						$(".player").fadeIn(2250);
+						$(".dealer").fadeIn(2250);
+						$(".startButton").css('display', 'inline-block');	
+						
+				  }, 2000);
 			}
 
-
-			$(document).ready(function(){
-			    $(".table").fadeIn(2250,playerStartAndStartButton());
-			});
-
 			function startGame() {
-				$(".startButton").hide();
+				$(".startButton").css("display",'none');
+				$(".hitButton").css('display', 'inline-block');
+				$(".standButton").css('display', 'inline-block');
+				$(".dealerCardButton").css('display', 'inline-block');
+				$(".dealerCardButton").append("<p>"+deck1.deal()+"</p>");
+
 			}
 
 		</script>
+
+
 
 		<style type="text/css">
 
@@ -38,6 +85,16 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 			.dealer{
 				display: none;
+		    position: relative;
+		    border-radius: 1em;
+		    border-color: cyan;
+		    border-style: solid;
+		    border-width: 0.7em;
+		    top: 6em;
+		    left: 0;
+		    height: 8em;
+		    width: 43em;
+		    margin: 0 auto 0 auto;
 			}
 
 			.player{
@@ -47,7 +104,7 @@ const char MAIN_page[] PROGMEM = R"=====(
 		    border-color: cyan;
 		    border-style: solid;
 		    border-width: 0.7em;
-		    top: 25em;
+		    top: 14em;
 		    left: 0;
 		    height: 8em;
 		    width: 43em;
@@ -193,7 +250,34 @@ const char MAIN_page[] PROGMEM = R"=====(
 			    position:relative;
 			    top:4px;
 			}
-
+			.dealerCardButton{
+				display: none;
+		    background: black;
+		    height: 53%;
+		    width: 15%;
+			  position:relative;
+			  color:#ecf0f1;
+			  text-decoration:none;
+			  border-radius:5px;
+			  border:solid 1px #f39c12;
+			  background:#e67e22;
+			  text-align:center;
+			  padding:16px 18px 14px;
+			  margin: 12px;
+			  -webkit-transition: all 0.1s;
+				-moz-transition: all 0.1s;
+				transition: all 0.1s;
+			  -webkit-box-shadow: 0px 6px 0px #d35400;
+			  -moz-box-shadow: 0px 6px 0px #d35400;
+			  box-shadow: 0px 6px 0px #d35400;
+			}
+			.dealerCardButton:active{
+			    -webkit-box-shadow: 0px 2px 0px #d35400;
+			    -moz-box-shadow: 0px 2px 0px #d35400;
+			    box-shadow: 0px 2px 0px #d35400;
+			    position:relative;
+			    top:4px;
+			}
 			.table{
 				display: none;
 		    height: 37em;
@@ -209,43 +293,32 @@ const char MAIN_page[] PROGMEM = R"=====(
 			}
 		</style>
 	</head>
-
 	<body>
 
 		<div class="table">
-			
 			<div class="dealer">
-				
+				<div class="dealerCardButton">
+				</div>
 			</div>
-
 			<div class="player">
-
 				<div class="startButton" onclick="startGame()">
 					<p>Start</p>
 				</div>
-
 				<div class="hitButton">
 					<p>Hit</p>
 				</div>
-
 				<div class="standButton">
 					<p>Stand</p>
 				</div>
-
 				<div class="splitButton">
 					<p>Split</p>
 				</div>
-
 				<div class="doubleDownButton">
 					<p>Double Down</p>
 				</div>
-
-				
 			</div>
-
 		</div>
 
 	</body>
-
 </html>
 )=====";
